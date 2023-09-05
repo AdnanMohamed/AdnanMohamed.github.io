d5ab0e7b713cfc93b37ba4754d6b61c8ed5a5386
Adnan Mohamed <adnan.hashem.bh@gmail.com>
2023-09-05 15:22:58 +0300

    Use javascript to align experience timeline cards instead of having the css class values inline.
    More Details:-
    Previously, the cards in the 'Work Experience' timeline were
    aligned such that one card is to the left and next to the right
    and so on using inline css. Now, I used JS function to set the
    cards class value. Why? Because then every time I include a new
    card for work experience, I will need to manually change all other
    card class values to maintain the left-then-right style.

---
+++ js/website.js
@@ -0,0 +1,24 @@
+// Summary: 
+//   Set each card's class value in the 'Experience' section
+//   such that the cards alternates between being in the left and
+//   right side of the timeline.
+//
+// Assumptions:
+//   1. Each card is a child of a div which has 'timeline' class.
+//   2. Each card does not currently have a class value which
+//      sets its side orientation.
+//   3. The class names 'left' and 'right' when assigned to the div element
+//      will align the div to the left or right respectively.
+function setCardsSide() {
+    const className = 'timeline';  // the class name which the container div has
+    const cards = document.querySelectorAll('div.'+className);
+    //console.log(cards);
+    for(let i = 0; i < cards.length; ++i) {
+        let side = i % 2 == 0 ? 'left':'right';
+        cards[i].classList.add(side);
+    }
+}
+
+// run the functions which must be executed before loading
+// the content of the document.
+setCardsSide();
