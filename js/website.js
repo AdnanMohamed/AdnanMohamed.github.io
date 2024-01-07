2227d0b0c560c0f5d807ff9e4167432e6b3c6b56
Adnan Mohamed <adnan.hashem.bh@gmail.com>
2024-01-07 09:38:02 +0300

    Minor changes

--- js/website.js
+++ js/website.js
@@ -1,4 +1,4 @@
-// Summary: 
+// Summary:
 //   Set each card's class value in the 'Experience' section
 //   such that the cards alternates between being in the left and
 //   right side of the timeline.
@@ -10,15 +10,23 @@
 //   3. The class names 'left' and 'right' when assigned to the div element
 //      will align the div to the left or right respectively.
 function setCardsSide() {
-    const className = 'timeline';  // the class name which the container div has
-    const cards = document.querySelectorAll('div.'+className);
-    //console.log(cards);
-    for(let i = 0; i < cards.length; ++i) {
-        let side = i % 2 == 0 ? 'left':'right';
-        cards[i].classList.add(side);
-    }
+  const className = "timeline"; // the class name which the container div has
+  const cards = document.querySelectorAll("div." + className);
+  //console.log(cards);
+  for (let i = 0; i < cards.length; ++i) {
+    let side = i % 2 == 0 ? "left" : "right";
+    cards[i].classList.add(side);
+  }
 }
 
 // run the functions which must be executed before loading
 // the content of the document.
 setCardsSide();
+
+// Enable tooltips
+document.addEventListener("DOMContentLoaded", function () {
+  var tooltips = document.querySelectorAll('[data-toggle="tooltip"]');
+  tooltips.forEach(function (tooltip) {
+    new bootstrap.Tooltip(tooltip);
+  });
+});
